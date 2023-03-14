29ed62cdaf2540f85d3d5b3099a2ca3a6723e4aa
Patrick Kenneally <patrick.kenneally@gmail.com>
2023-03-13 23:56:36 -0600

    Initial commit of default empty project

---
+++ Source/ceilim/ceilim.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ceilim.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ceilim, "ceilim" );
