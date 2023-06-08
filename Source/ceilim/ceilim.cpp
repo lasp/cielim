4e8a77ca068e275dc3eefe2f5bdaea97b00ce068
Patrick Kenneally <patrick.kenneally@gmail.com>
2023-06-07 20:37:32 -0600

    Correct project file spelling

--- Source/ceilim/ceilim.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ceilim.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ceilim, "ceilim" );
---
+++ Source/cielim/cielim.Build.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+
+public class cielim : ModuleRules
+{
+	public cielim(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "ProtobufLibrary" });
+	}
+}
