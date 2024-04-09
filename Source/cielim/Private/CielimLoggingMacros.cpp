57a735980eb295e5c796fb33a35b0b2b585312f7
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-04-08 19:25:01 -0600

    Add cielim logging macro

---
+++ Source/cielim/Private/CielimLoggingMacros.cpp
@@ -0,0 +1,3 @@
+#include "CielimLoggingMacros.h"
+
+DEFINE_LOG_CATEGORY(LogCielim);
---
+++ Source/cielim/Public/CielimLoggingMacros.h
@@ -0,0 +1,6 @@
+// Defines logging categories for the project to ease parsing the log files.
+// It's usually a good idea to make a new log category for every major system in the project,
+// like a procedural level generator, or perhaps for your game mode.
+#pragma once
+
+DECLARE_LOG_CATEGORY_EXTERN(LogCielim, Log, All);
