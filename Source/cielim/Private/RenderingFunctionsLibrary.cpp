b74a66fa327b17c73d425954b9adeda97b14298f
Karl Mudsam <karl@boldergames.com>
2024-05-28 11:54:17 -0600

    Create blueprint rendering pipeline
    -  with empty functions ApplyPSFGaussian, ApplyReadNoise, ApplyCosmicRays, and WriteImage

---
+++ Source/cielim/Private/RenderingFunctionsLibrary.cpp
@@ -0,0 +1,28 @@
+
+#include "RenderingFunctionsLibrary.h"
+
+#include <OpenCV/PreOpenCVHeaders.h>
+#include <opencv2/core.hpp>
+#include <OpenCV/opencv-4.5.5/modules/imgcodecs/include/opencv2/imgcodecs.hpp>
+#include <OpenCV/PostOpenCVHeaders.h>
+
+URenderingFunctionsLibrary::URenderingFunctionsLibrary(const FObjectInitializer& ObjectInitializer)
+{
+
+}
+
+void URenderingFunctionsLibrary::ApplyPSF_Gaussian()
+
+{
+	
+}
+
+void URenderingFunctionsLibrary::ApplyCosmicRays()
+{
+	
+}
+
+void URenderingFunctionsLibrary::ApplyReadNoise()
+{
+	
+}
---
+++ Source/cielim/Private/RenderingFunctionsLibrary.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Math/Vector.h"
+#include "Engine/World.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include <OpenCV/PreOpenCVHeaders.h>
+#include "OpenCV/opencv-4.5.5/modules/imgcodecs/include/opencv2/imgcodecs.hpp"
+#include <OpenCV/PostOpenCVHeaders.h>
+
+#include "RenderingFunctionsLibrary.generated.h"
+
+UCLASS()
+class  CIELIM_API URenderingFunctionsLibrary :
+	public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	URenderingFunctionsLibrary(const FObjectInitializer& ObjectInitializer);
+	
+	UFUNCTION(BlueprintCallable, Category="Rendering Functions")
+	static void ApplyPSF_Gaussian();
+
+	UFUNCTION(BlueprintCallable, Category="Rendering Functions")
+	static void WriteImage();
+
+	UFUNCTION(BlueprintCallable, Category="Rendering Functions")
+	static void ApplyCosmicRays();
+
+	UFUNCTION(BlueprintCallable, Category="Rendering Functions")
+	static void ApplyReadNoise();
+
+	static void ReadImage(){};
+
+	
+
+	
+};
+
+
+
