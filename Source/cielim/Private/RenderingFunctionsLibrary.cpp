3425ac97d12b7bd6cdc2b56f9f8de39be76eac3a
Karl Mudsam <karl@mudsam.com>
2024-05-29 13:53:16 -0600

    Populate the ApplyPSF_Gaussian function

--- Source/cielim/Private/RenderingFunctionsLibrary.cpp
+++ Source/cielim/Private/RenderingFunctionsLibrary.cpp
@@ -1,75 +1,58 @@
-
 #include "RenderingFunctionsLibrary.h"
 
 #include <OpenCV/PreOpenCVHeaders.h>
 #include <opencv2/core.hpp>
+#include "opencv2/imgproc.hpp"
 #include <OpenCV/opencv-4.5.5/modules/imgcodecs/include/opencv2/imgcodecs.hpp>
 #include <OpenCV/PostOpenCVHeaders.h>
 
+#include <filesystem>
+#include "CielimLoggingMacros.h"
+
+
 URenderingFunctionsLibrary::URenderingFunctionsLibrary(const FObjectInitializer& ObjectInitializer)
 {
-
-}
-
-void URenderingFunctionsLibrary::ApplyPSF_Gaussian()
-
-{
 	
 }
 
-void URenderingFunctionsLibrary::ApplyCosmicRays()
+FString URenderingFunctionsLibrary::ApplyPSF_Gaussian(FString Filepath, int32 KernelHeight, int32 KernelWidth, double SigmaX, double SigmaY)
 {
-	
-}
-
-FString URenderingFunctionsLibrary::ApplyReadNoise(FString Filepath, float ReadNoiseSigma, float SystemGain)
-{
-	//Protect Against 0 Sigma
-	if(ReadNoiseSigma == 0)
-	{
-		return Filepath;
-	}
-	
+	//NOTE: both dimensions of KernelSize must be odd
 	//Read Image
+
 	FString Filepath_Absolute = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*Filepath);
+	
 	std::string Filepath_Absolute_String = TCHAR_TO_UTF8(*Filepath_Absolute);
 	cv::Mat Image = cv::imread(Filepath_Absolute_String);
-	
-	//Init and create noise matrix
-	cv::Mat GaussianNoise = cv::Mat(Image.rows, Image.cols, Image.type());
-	cv::randn(GaussianNoise, 0, ReadNoiseSigma);
 
-	//Init Resulting image
-	cv::Mat ResultImage = cv::Mat(Image.rows, Image.cols, Image.type());
+	//Init ResultImage
+	cv::Mat ResultImage;
 
-	//Apply Noise
-	ResultImage = Image + (SystemGain * GaussianNoise);
+	//Get kernel size information into a nice format
+	cv::Size KernelSize = cv::Size(KernelHeight, KernelWidth);
 
-	//Clamp values to [0,255]
-	//Separate color channels
-	std::vector<cv::Mat> DifferentColorChannels;
-	cv::split(ResultImage, DifferentColorChannels);
-
-	//Init LowerBound and UpperBound matrices
-	cv::Mat LowerBoundMatrix = cv::Mat::zeros(Image.rows, Image.cols, DifferentColorChannels[0].type());
-	cv::Mat UpperBoundMatrix = cv::Mat::ones(Image.rows, Image.cols, DifferentColorChannels[0].type()) * 255;
-
-	//Clamp
-	for(int ColorChannel =0; ColorChannel < 3; ColorChannel++)
-	{
-		cv::min(cv::max(DifferentColorChannels[ColorChannel], LowerBoundMatrix), UpperBoundMatrix, DifferentColorChannels[ColorChannel]);
-	}
-
-	//Merge back into a color image
-	cv::merge(DifferentColorChannels, ResultImage);
+	//Apply blur
+	cv::GaussianBlur(Image, ResultImage, KernelSize, SigmaX, SigmaY);
 
 	//Save image
 	FString ResultFilepath = FPaths::ProjectDir();
 	ResultFilepath.Append("Result_Images/");
-	ResultFilepath.Append("ReadNoise.jpg");
+	ResultFilepath.Append("PSF.jpg");
 	
 	std::string ResultFilepath_String = TCHAR_TO_UTF8(*ResultFilepath);
 	cv::imwrite(ResultFilepath_String, ResultImage);
 	
 	return ResultFilepath;
+}
+
+void URenderingFunctionsLibrary::ApplyCosmicRays()
+{
+	//Do Not Call
+	throw std::logic_error("Function not yet implemented");
+}
+
+void URenderingFunctionsLibrary::ApplyReadNoise()
+{
+	//Do Not Call
+	throw std::logic_error("Function not yet implemented");
 }
