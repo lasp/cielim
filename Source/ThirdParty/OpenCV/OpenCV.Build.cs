b094725ab7c770f1670ae5f9f38da98416ad6b79
Owen Allison <owal7428@colorado.edu>
2024-09-15 19:49:41 -0600

    Update Build.cs files to support windows library file types

--- Source/ThirdParty/OpenCV/OpenCV.Build.cs
+++ Source/ThirdParty/OpenCV/OpenCV.Build.cs
@@ -8,11 +8,6 @@ public class OpenCV : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	    bAddDefaultIncludePaths = false;
         
-        if (Target.Platform != UnrealTargetPlatform.Mac)
-        { 
-            string Err = string.Format("OpenCV library not available for platform {0}", Target.Platform.ToString());
-            throw new BuildException(Err);
-        }
         
         PublicDefinitions.Add("_ALLOW_MSC_VER_MISMATCH");
 
@@ -20,10 +15,25 @@ public class OpenCV : ModuleRules
         PrecompileForTargets = PrecompileTargetsType.Any;
 
         // Link program to OpenCV Library files (these should all be included in libopencv_world)
-        PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "opencv/build/lib/libopencv_world.dylib"));
+        if (Target.Platform == UnrealTargetPlatform.Mac || Target.Platform == UnrealTargetPlatform.Linux)
+        {
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "opencv/build/lib/libopencv_world.dylib"));
 
-        // Add include path for OpenCV headers
-        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "opencv/include/opencv4/opencv2"));
+            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "opencv/include/opencv4/opencv2"));
+        }
+        else if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "opencv/build/lib/Release/opencv_world4100.lib"));
+
+            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "opencv/include/opencv2"));
+        }
+        else
+        { 
+            string Err = string.Format("OpenCV library not available for platform {0}", Target.Platform.ToString());
+            throw new BuildException(Err);
+        }
+
+        // Add universal include path for OpenCV headers
         PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "opencv/modules/core/include"));
         PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "opencv/modules/imgproc/include"));
         PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "opencv/build"));
