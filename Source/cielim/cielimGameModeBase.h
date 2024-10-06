e2a2a6dfc448bcb4207a2472f3c81ea54e050f1a
Owen Allison <owal7428@colorado.edu>
2024-10-06 13:40:01 -0600

    Added startup logging for debug purposes

--- Source/cielim/cielimGameModeBase.h
+++ Source/cielim/cielimGameModeBase.h
@@ -6,12 +6,20 @@
 #include "GameFramework/GameModeBase.h"
 #include "cielimGameModeBase.generated.h"
 
-/**
- * 
- */
+// GameMode class for cielim
 UCLASS()
 class CIELIM_API AcielimGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Constructor (not currently in use at the moment)
+	AcielimGameModeBase();
+
+protected:
+	// Initial game setup
+	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+
+	// This is where game logic begins execution
+	virtual void BeginPlay() override;
 };
