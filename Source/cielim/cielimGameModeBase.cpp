bd207a4515c63b32942c977fc5ef5884a4633894
Owen Allison <owal7428@colorado.edu>
2024-10-06 13:40:01 -0600

    Added startup logging for debug purposes

--- Source/cielim/cielimGameModeBase.cpp
+++ Source/cielim/cielimGameModeBase.cpp
@@ -1,5 +1,26 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-
 #include "cielimGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 
+#include "CielimLoggingMacros.h"
+
+AcielimGameModeBase::AcielimGameModeBase()
+{
+    // This doesn't do anything for now
+}
+
+void AcielimGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+    Super::InitGame(MapName, Options, ErrorMessage);
+
+    UE_LOG(LogCielim, Log, TEXT("Cielim is now initialized..."));
+}
+
+void AcielimGameModeBase::BeginPlay()
+{
+    Super::BeginPlay();
+
+    UE_LOG(LogCielim, Log, TEXT("Cielim is now starting..."));
+}
