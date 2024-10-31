19de6d40eeab757b4103d36f4842a0e89038fb08
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-10-31 00:04:44 -0600

    Add center of brightness request test

---
+++ Source/test/test_request_center_of_brightness.py
@@ -0,0 +1,70 @@
+import delimited_protobuf
+import pytest
+import numpy as np
+
+from test_harness import *
+import cielimMessage_pb2
+
+
+@pytest.fixture
+def scene_setup():
+    protobuf_message = cielimMessage_pb2.CielimMessage()
+        
+    body = protobuf_message.celestialBodies.add()
+    body.bodyName = "asteroid"
+    [body.position.append(item) for item in [0,0,0]]
+    [body.attitude.append(item) for item in [0,0,0]]
+    
+    body.models.shapeModel = "sphere_normalized"
+    body.models.meanRadius = 10000
+    
+    sun = protobuf_message.celestialBodies.add()
+    sun.bodyName = "sun_planet_data"
+    [sun.position.append(item) for item in [0,0,-10000]]
+    [sun.attitude.append(item) for item in [0,0,0]]
+    
+    protobuf_message.camera.cameraId = 1
+    protobuf_message.camera.parentName = "cielim_sat"
+    [protobuf_message.camera.fieldOfView.append(item) for item in [20*np.pi/180, 15*np.pi/180]]
+    [protobuf_message.camera.bodyFrameToCameraMrp.append(item) for item in [0,0,0]]
+    [protobuf_message.camera.cameraPositionInBody.append(item) for item in [1,1,1]]
+    [protobuf_message.camera.resolution.append(item) for item in [4000, 3000]]
+    
+    protobuf_message.spacecraft.spacecraftName = "cielim_sat"
+    [protobuf_message.spacecraft.position.append(item) for item in [0,0,-1000000]]
+    [protobuf_message.spacecraft.attitude.append(item) for item in [0, 0, 0]]
+    return protobuf_message
+    
+
+def test_request_image_and_center_of_brightness(scene_setup):
+    connector = Connector()
+    connector.connect("tcp://127.0.0.1:5556")
+
+    connector.send_frame(scene_setup)
+    [image, center_of_brightness] = connector.request_image_for_camera_id(1, 1)
+    height, width, channels = image.shape
+    np.testing.assert_allclose([4000, 3000], [width, height], rtol=0, atol=0, err_msg='Returned image not correct')
+
+    true_center_of_brightness = [1499.5, 1999.5]
+    np.testing.assert_allclose(center_of_brightness, 
+                               true_center_of_brightness, 
+                               rtol=1e-1, 
+                               atol=0,
+                               err_msg='Center of brightness not close enough to expected')
+
+
+def test_request_only_center_of_brightness(scene_setup):
+    connector = Connector()
+    connector.connect("tcp://127.0.0.1:5556")
+
+    connector.send_frame(scene_setup)
+    [image, center_of_brightness] = connector.request_image_for_camera_id(1, 0)
+    
+    assert image == None
+
+    true_center_of_brightness = [1499.5, 1999.5]
+    np.testing.assert_allclose(center_of_brightness, 
+                               true_center_of_brightness, 
+                               rtol=1e-1, 
+                               atol=0,
+                               err_msg='Center of brightness not close enough to expected')
