308e252be05d906159370eeb1d755ac769c41d52
Owen Allison <owal7428@colorado.edu>
2024-11-02 22:57:11 -0600

    Cleaned up test demo script

--- Source/test/test_demo.py
+++ Source/test/test_demo.py
@@ -9,17 +9,22 @@ if __name__ == "__main__":
     
     file_handler = MessageFileHandler(file_dir + file_name)
 
-    connector.send_frame(file_handler.get_simulation_frame_at_time(0.0))
-    connector.send_frame(file_handler.get_next_simulation_frame())
+    idx = 0
+    image = None
+
+    while True:
+        frame = file_handler.get_next_simulation_frame()
+
+        if (frame is None):
+            break
+
+        connector.send_frame(frame)
+        image = connector.request_image_for_camera_id(1)
+
+        cv2.imwrite("received_image_" + str(idx) + ".png", image)
+
+        idx = idx + 1
     
-    message = None
-    while message is not None:
-        message = file_handler.get_next_simulation_frame()
-        connector.send_frame(message)
-
-    image = connector.request_image_for_camera_id(1)
-    cv2.imwrite("received_image.png", image)
-
     cv2.namedWindow("window_name", cv2.WINDOW_NORMAL)
     cv2.imshow("window_name", image)
     cv2.resizeWindow("window_name", 640, 480)
