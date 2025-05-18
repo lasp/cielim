0289ad787a8a25e6b5df7e00c5856741f131af0c
tteil <thibaud.teil@lasp.colorado.edu>
2025-05-17 20:38:30 -0600

    modify print proto for test

--- Source/cielim-python/examples/print_protobuffer_content.py
+++ Source/cielim-python/examples/print_protobuffer_content.py
@@ -1,16 +1,22 @@
 import os
+import glob
 from context import cielimMessage_pb2
 import delimited_protobuf
 
-if __name__ == "__main__":
-    file_dir = os.path.dirname(__file__) + "/../../../Content/FlybyData/bin/"
-    file_name = input("What is the bin file to test (name only): ")
+def print_protobuffer_content():
+    file_dir = os.path.join(os.path.dirname(__file__), "../../../Content/FlybyData/bin/")
+    file_name = "bennu_image.bin"
+    file_path = os.path.join(file_dir, file_name)
 
-    file_handle = open(file_dir + file_name, "rb")
+    with open(file_path, "rb") as file_handle:
+        message = delimited_protobuf.read(file_handle, cielimMessage_pb2.CielimMessage)
 
-    message = delimited_protobuf.read(file_handle, cielimMessage_pb2.CielimMessage)
-
-    output_name = file_name[:-4] + "_decoded.txt"
+    output_name = os.path.splitext(file_name)[0] + "_decoded.txt"
 
     with open(output_name, "w") as out_file:
         print(message, file=out_file)
+
+    return output_name, message
+
+if __name__ == "__main__":
+    print_protobuffer_content()
