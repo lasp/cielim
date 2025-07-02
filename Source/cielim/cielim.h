4b56f3ce93b493e4e37bd8252ad672ea7e6c3c32
Owen Allison <owal7428@colorado.edu>
2025-07-01 22:11:49 -0600

    Use custom cielim module class and register custom shader directory

--- Source/cielim/cielim.h
+++ Source/cielim/cielim.h
@@ -1,6 +1,19 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+//=================== Copyright (c) 2025 Laboratory for Atmospheric and Space Physics ===================//
+//
+// Purpose: Defines the FCielimModule class which is used to implement the Cielim module that is
+//          recognized by Unreal Engine.
+//
+// License: MIT License. See LICENSE file.
+//
+//=======================================================================================================//
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
 
+class FCielimModule final : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
