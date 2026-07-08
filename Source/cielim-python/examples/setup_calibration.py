effab75bc4ae6e394270a60c97b9ff5fd1dd88f8
Owen Allison <94411568+owal7428@users.noreply.github.com>
2026-07-08 00:46:49 -0600

    Update calibration script

--- Source/cielim-python/examples/setup_calibration.py
+++ Source/cielim-python/examples/setup_calibration.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as np
+import math
 
 import cielim
 
@@ -7,9 +7,10 @@ import cielim
 def scene_setup() -> cielim.Scene:
     scene = cielim.Scene()
 
-    scene.set_spacecraft_params(position=(0, 0, 2000), attitude=(0, 1, 0))
+    scene.set_spacecraft_params(position=(0, 0, 200), attitude=(0, 1, 0))
 
-    scene.set_lens_params(fov=(10 * np.pi / 180, 10 * np.pi / 180))
+    scene.set_lens_params(fov=(3.1415 * 110 / 180, 3.1415 * 110 / 180))
+    scene.set_sensor_params(exposure=5e-9)
 
     index = scene.add_celestial_body("asteroid")
     scene.set_celestial_body_params(index, mesh_shape="Plane", mesh_brdf="Lambertian", mesh_radius=1000)
@@ -23,22 +24,29 @@ if __name__ == "__main__":
     """
 
     connector = cielim.Connector()
-    launcher = cielim.Launcher()
-    connector.connect(launcher.launch())
+    # launcher = cielim.Launcher()
+    # connector.connect(launcher.launch())
+    connector.connect()
+
     connector.send_init_request()
 
     scene = scene_setup()
 
-    connector.send_init_request()
-    connector.send_frame(scene.get_scene())
-    image, _, _ = connector.request_image_for_camera_id(1, True, False)
+    rotations = 1
 
-    WindowName = f"Image_Client_{connector.identity}"
-    cv2.namedWindow(WindowName, cv2.WINDOW_NORMAL)
-    cv2.imshow(WindowName, image)
-    cv2.resizeWindow(WindowName, 1000, 1000)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    for i in range(rotations + 1):
+        theta = math.pi * (1 - i / rotations)
+        scene.set_spacecraft_params(attitude=(0, math.tan(theta / 4), 0))
+
+        connector.send_frame(scene.get_scene())
+        image, _, _ = connector.request_image_for_camera_id(1, True, False)
+
+        WindowName = f"Image_Client_{connector.identity}"
+        cv2.namedWindow(WindowName, cv2.WINDOW_NORMAL)
+        cv2.imshow(WindowName, image)
+        cv2.resizeWindow(WindowName, 1000, 1000)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     connector.disconnect()
-    launcher.terminate()
+    # launcher.terminate()
